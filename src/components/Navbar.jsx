d4f5e576834b056320febbdf49751979bcf2a8a5
H M Rizwan Mazumder <42119262+xtremist96@users.noreply.github.com>
2023-03-25 01:05:57 +0530

    complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,8 +22,11 @@ const Navbar = () => {
         }}
         >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Rizwan
-          <span className='sm:block hidden'> | Techno3Gamma</span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Rizwan &nbsp;
+          {/* <span className='sm:block hidden'>  */}
+          | Techno3Gamma
+          {/* </span> */}
+          </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           { navLinks.map((link) => (
