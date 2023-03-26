5d035fb95caaf0cf3a4f8d1d7331f27117d0e0b5
H M Rizwan Mazumder <42119262+xtremist96@users.noreply.github.com>
2023-03-27 01:45:15 +0530

    link fix & new components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom"
-import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, FloatingHero, StarsCanvas} from './components';
 
 const App = () => {
   return (
@@ -7,8 +7,10 @@ const App = () => {
     <div className="relative z-0 bg-primary">
       <div className="bg-hero pattern bg-cover bg-no-repeat bg-center">
         <Navbar/>
+        
         <Hero />
       </div>
+      
       <About />
       <Experience />
       <Tech />
@@ -18,7 +20,9 @@ const App = () => {
         <Contact />
         <StarsCanvas />
       </div>
-    </div>
+      
+      {/* <div className="relative z-0"><FloatingHero /></div> */}
+      </div>
     </BrowserRouter>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -7,9 +7,11 @@ import Experience from './Experience';
 import Works from './Works';
 import Feedbacks from './Feedbacks';
 import Contact from './Contact';
+import FloatingHero from './FloatingHero';
 
 export {
   Hero,
+  FloatingHero,
   Navbar,
   About,
   Tech,
